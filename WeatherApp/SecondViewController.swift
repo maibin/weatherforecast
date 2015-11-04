1fc69bdb883ed25c00372f19daebeba2bdb41f50
michal_aibin <michal.aibin@gmail.com>
2015-11-03 23:32:12 -0800

    Initial Commit

---
+++ WeatherApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeatherApp
+//
+//  Created by Michał Aibin on 03.11.2015.
+//  Copyright © 2015 CodeRunner. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
